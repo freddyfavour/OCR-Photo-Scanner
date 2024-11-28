4b3a8da39f1293bb98bee4e8b99882a9fcbbfb1a
Favour Alfred <alfredfavour76@gmail.com>
2024-11-28 16:25:33 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBabwddYGAXWY8Y9xSn1auNAbOfO7hxK0o",
-  authDomain: "res-build.firebaseapp.com",
-  projectId: "res-build",
-  storageBucket: "res-build.appspot.com",
-  messagingSenderId: "755096563596",
-  appId: "1:755096563596:web:0a700794600ae6430db0a9",
-  measurementId: "G-R8ZF530SX1",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
